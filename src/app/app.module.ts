3ad29cf578dc9170f6446d01678231b26d8d49c6
ubuntu-orca <webmasterbengrich@gmail.com>
2017-08-04 11:00:58 +0100

    finish crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,25 @@ import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
 
 const ROUTES = [
   { path: '', redirectTo: 'books', pathMatch: 'full' },
-  { path: 'books', component: BookComponent }
+  { path: 'books', component: BookComponent },
+  { path: 'book-details/:id', component: BookDetailComponent },
+  { path: 'book-create', component: BookCreateComponent },
+  { path: 'book-edit/:id', component: BookEditComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    BookDetailComponent,
+    BookCreateComponent,
+    BookEditComponent
   ],
   imports: [
     BrowserModule,
